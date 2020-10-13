7a4c90e9491f8f5851616233e75ed614a6388ed5
dev1980 <brham1980@gmail.com>
2020-10-13 18:49:51 +0545

    add component

---
+++ gatsby-travel/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from 'react'
+import { FaBars, Fatimes } from "react-icons/fa"
+import { IconsContext } from "react-icons/lib"
+
+const Navbar = () => {
+return(
+  <>
+
+  </>
+)
+}
+
+export default Navbar
--- gatsby-travel/src/pages/index.js
+++ gatsby-travel/src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
